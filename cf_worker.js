90990745df14f72ec8999c4d7ea7a751a78cd71c
RealDream <157290703+qiaozishun4@users.noreply.github.com>
2025-07-21 14:16:16 +0800

    Update cf_worker.js

--- cf_worker.js
+++ cf_worker.js
@@ -1,4 +1,4 @@
-const TELEGRAPH_URL = 'https://api.openai.com';
+const TELEGRAPH_URL = 'https://free.v36.cm';
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
